6988a2679a352ba0319768de01ab0f856e12690a
Mariya Buzhava <bujava.masha@gmail.com>
2024-11-02 18:47:50 +0200

    imagecard.tsx

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -1,6 +1,12 @@
+import { Image } from "../App/App.types";
 import c from "./ImageCard.module.css";
 
-const ImageCard = ({ image, onClick }) => {
+interface ImageCardProps {
+  image: Image;
+  onClick: () => void;
+}
+
+const ImageCard: React.FC<ImageCardProps> = ({ image, onClick }) => {
   return (
     <div className={c.imageCard} onClick={onClick}>
       <img src={image.urls.small} alt={image.alt_description || "Image"} />
