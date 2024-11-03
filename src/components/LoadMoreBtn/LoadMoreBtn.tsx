dd344b198c48dcb61600e62bb8b7f867e49b3a6b
Mariya Buzhava <bujava.masha@gmail.com>
2024-11-03 22:12:51 +0200

    loadmorebtn.tsx

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,6 +1,10 @@
 import c from "./LoadMoreBtn.module.css";
 
-const LoadMoreBtn = ({ onClick }) => {
+interface LoadMoreBtnProps {
+  onClick: () => void;
+}
+
+const LoadMoreBtn: React.FC<LoadMoreBtnProps> = ({ onClick }) => {
   return (
     <div>
       <button onClick={onClick} className={c.button}>
